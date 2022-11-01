08e8daa4a9c6114de9f94e166de0364eff384336
evilapels <105581504+pewish@users.noreply.github.com>
2022-11-01 21:24:26 +0300

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,18 +1,19 @@
 function checkSpam(str) {
-        var firstWord = 'viagra';
-        var secondWord = 'xxx';
-        var string = str.toLowerCase();
-        var result;
+        //По заданию нужно "поймать" слова с viagra и с xxx, поэтому объявим такие переменные
+        var firstWord = 'viagra';  //Объявление символьной переменной
+        var secondWord = 'xxx'; //Объявление символьной переменной
+        var string = str.toLowerCase(); //Возвращение значения строки, преобразованное в нижний регистр
+        var result; //Сюда будем записывать вводимые слова
 
         if (checkTheWord(firstWord) || checkTheWord(secondWord)) {
             return true;
         }
         return false;
 
-        function checkTheWord(word) {
-            for (var i = 0; i < string.length - 1; i++) {
-                if (string[i] == word[0]) {
-                    result = string.slice(i, i + word.length);
+        function checkTheWord(word) { //Создадим функцию для проверки слова
+            for (var i = 0; i < string.length - 1; i++) { //Проходим по массиву (string.length - длина слова)
+                if (string[i] == word[0]) { //"Побуквенно" сравниваем
+                    result = string.slice(i, i + word.length); //slice() возвращает новый массив, содержащий копию части исходного массива
                     if (result == word) {
                         return true;
                     }
@@ -20,3 +21,8 @@ function checkSpam(str) {
             }
         }
     }
+
+//Вывод в консоль
+console.log(checkSpam('buy ViAgRA now'));
+console.log(checkSpam('free xxxxx'));
+console.log(checkSpam("innocent rabbit"));
